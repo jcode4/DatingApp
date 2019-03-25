f5b444a954b90b0f0e1ff85c2c1468dbcb23b0c8
JSamson <joseph.samson@cbre.com>
2019-03-25 16:24:26 -0500

    Added Repository Pattern, Password Hash/Salt

--- DatingApp.API/Data/IAuthRepository.cs
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -1,7 +1,14 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
 namespace DatingApp.API.Data
 {
-    public class IAuthRepository
+    public interface IAuthRepository
     {
-        
+        Task<User> Register(User user, string password);
+
+        Task<User> Login(string username, string password);
+
+        Task<bool> UserExists(string username);
     }
 }
